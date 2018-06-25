f86463ecc301e587dfa4c392c0e23d2a8d160898
nigeon <nigeon@gmail.com>
2018-06-25 16:03:03 +0200

    Suing an enum for Bet status. Random Bugfixing.

--- contracts/BetFactory.sol
+++ contracts/BetFactory.sol
@@ -10,12 +10,14 @@ contract BetFactory is Ownable {
   event NewBet(uint betId, address owner, string title, uint entryPrice);
   event NewEntry(uint betId, uint entryId, address owner, string content, uint price);
 
+  enum BetStatus { active, stopped, ended, assigned, done } //ACTIVE <-> STOPPED -> ENDED -> ASSIGNED -> DONE
+
   struct Bet {
     address owner;
     string title;
     uint entryPrice;
     uint raised;
-    string status; //Possible Statuses: ACTIVE <-> STOPPED -> ENDED -> ASSIGNED -> DONE
+    BetStatus status;
     uint[] entries;
     string winner_content;
     uint[] winners;
@@ -39,7 +41,7 @@ contract BetFactory is Ownable {
   }
 
   function createBet(string _title, uint _entryPrice) public returns(uint) {
-    uint _betId = bets.push(Bet(msg.sender, _title, _entryPrice, 'ACTIVE', new uint[](0), '', new uint[](0), 0)).sub(1);
+    uint _betId = bets.push(Bet(msg.sender, _title, _entryPrice, 0, BetStatus.active, new uint[](0), '', new uint[](0))).sub(1);
     betToOwner[_betId] = msg.sender;
     emit NewBet(_betId, msg.sender, _title, _entryPrice);
 
@@ -47,7 +49,7 @@ contract BetFactory is Ownable {
   }
 
   function enter(uint _betId, string _content) public payable returns(uint) {
-    require(bets[_betId].status == 'ACTIVE');
+    require(bets[_betId].status == BetStatus.active);
     require(msg.value == bets[_betId].entryPrice);
     
     bets[_betId].raised.add(msg.value);
@@ -60,57 +62,45 @@ contract BetFactory is Ownable {
   }
 
   function close(uint _betId) public onlyOwnerOf(_betId){
-    bets[_betId].status = 'STOPPED';
+    bets[_betId].status = BetStatus.stopped;
+
   }
 
   function finish(uint _betId, string _winner_content) public onlyOwnerOf(_betId){
-    require(bets[_betId].status == 'STOPPED');
+    require(bets[_betId].status == BetStatus.stopped);
     
-    bets[_betId].status = 'ENDED'
+    bets[_betId].status = BetStatus.ended;
     bets[_betId].winner_content = _winner_content;
   }
 
   function assignPrices(uint _betId) public onlyOwnerOf(_betId){
-    require(bets[_betId].status == 'ENDED');
+    require(bets[_betId].status == BetStatus.ended);
 
-    bets[_betId].status = 'ASSIGNED';
+    bets[_betId].status = BetStatus.assigned;
 
     //Getting the winners looping through entries
-    for (uint i = 0; i < bets[_betId].length; i++) {  
-      if(bets[_betId].entries[i].content == bets[_betId].winner_content){
-        winners.push(i);
+    for (uint i = 0; i < bets[_betId].entries.length; i++) {  
+      uint _entryId = bets[_betId].entries[i];
+      if(keccak256(entries[_entryId].content) == keccak256(bets[_betId].winner_content)){
+        bets[_betId].winners.push(i);
       }
     }
   }
 
   function sendPrices(uint _betId) public onlyOwnerOf(_betId){
-    require(bets[_betId].status == 'ASSIGNED');
+    require(bets[_betId].status == BetStatus.assigned);
 
-    bets[_betId].status = 'DONE';
+    bets[_betId].status = BetStatus.done;
 
     if(bets[_betId].winners.length > 0){
-      uint each_prize = raised.div(bets[_betId].winners.length);
+      uint each_prize = bets[_betId].raised.div(bets[_betId].winners.length);
 
       for(uint i = 0; i < bets[_betId].winners.length; i++){
-        require(bets[_betId].entries[bets[_betId].winners[i]].owner.send(each_prize));
+        uint _entryId = bets[_betId].winners[i];
+        require(entries[_entryId].owner.send(each_prize));
       }
     }
 
   }
 
-  /**
-   * Struct UnPacker
-   * TODO: IS THIS ONLY NEEDED/USED IN THE SOLIDITY TESTS?
-   */
-  function getBet(uint _betId) external view returns (address, string, uint, bool, bool, bool, string) {
-    return (bets[_betId].owner, bets[_betId].title, bets[_betId].entryPrice, bets[_betId].active, bets[_betId].finished, bets[_betId].prices_sent, bets[_betId].winner_content);
-  }
-
-  /**
-   * Struct UnPacker
-   * TODO: IS THIS ONLY NEEDED/USED IN THE SOLIDITY TESTS?
-   */
-  function getEntry(uint _entryId) external view returns (uint, address, string, uint) {
-    return (entries[_entryId].betId, entries[_entryId].owner, entries[_entryId].content, entries[_entryId].price);
-  }
 }
