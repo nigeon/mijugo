f52a8a35760f99b619898519b330ffc3638852d2
nigeon <nigeon@gmail.com>
2018-06-28 14:27:43 +0200

    Unboxed Drizzle box, moved everything here.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
+var SimpleStorage = artifacts.require("SimpleStorage");
+var TutorialToken = artifacts.require("TutorialToken");
+var ComplexStorage = artifacts.require("ComplexStorage");
 var BetFactory = artifacts.require("./BetFactory.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(SimpleStorage);
+  deployer.deploy(TutorialToken);
+  deployer.deploy(ComplexStorage);
   deployer.deploy(BetFactory);
 };
